1b8ff620631a0a069d79037a9725060029557852
martinpro1000 <92687407+martinpro1000@users.noreply.github.com>
2021-12-21 17:40:03 +0100

    Create Clicker.java

---
+++ Clicker.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Clicker implements ActionListener {
+
+    //OPEN SOURCE PROGRAM BY GITHUB: martinpro1000
+    
+    private JLabel numberLabel;
+    private int clicks = 0;
+    public void createAndShow() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); //optional
+            JFrame frame = new JFrame();
+            this.numberLabel = new JLabel("number of clicks:");
+            JButton clicker = new JButton("CLICK ME!");
+            clicker.setPreferredSize(new Dimension(100, 35));
+            clicker.addActionListener(this);
+            frame.add(clicker, BorderLayout.CENTER);
+            frame.add(numberLabel, BorderLayout.SOUTH);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setResizable(false); //optional
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        }catch(Exception e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Clicker().createAndShow();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        clicks += 1;
+        numberLabel.setText("number of clicks: " + clicks);
+        numberLabel.setVisible(true);
+    }
+}
